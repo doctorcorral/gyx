07abdb6f4573d6b95767008579480a24622090b2
Ricardo C <ricardo@suggestic.com>
2018-11-22 16:28:42 -0600

    add codebase

---
+++ lib/gyx.ex
@@ -0,0 +1,18 @@
+defmodule Gyx do
+  @moduledoc """
+  Documentation for Gyx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gyx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gyx_test.exs
@@ -0,0 +1,8 @@
+defmodule GyxTest do
+  use ExUnit.Case
+  doctest Gyx
+
+  test "greets the world" do
+    assert Gyx.hello() == :world
+  end
+end
