891fbc39209b70ea3def6c7ac61d9b139f24fd6d
Ricardo C <ricardo@suggestic.com>
2018-11-24 16:53:00 -0600

    add get_action(%Abstraction{})

--- lib/agents/blackjack_agent.ex
+++ lib/agents/blackjack_agent.ex
@@ -19,12 +19,35 @@ defmodule Agents.BlackjackAgent do
     GenServer.call(__MODULE__, {:q_set, {env_state, action, value}})
   end
 
-  def handle_call({:q_get, {env_state = %Abstraction{}, action}}, _from, state) do
+  def get_action(env_state = %Abstraction{}) do
+    GenServer.call(__MODULE__, {:get_action, env_state})
+  end
+
+  def handle_call(
+        {:q_get, {env_state = %Abstraction{}, action}},
+        _from,
+        state = %Agents.BlackjackAgent{}
+      ) do
     {:reply, state.state_value_table[inspect(env_state)][inspect(action)], state}
   end
 
-  def handle_call({:q_set, {env_state = %Abstraction{}, action, value}}, _from, state) do
+  def handle_call(
+        {:q_set, {env_state = %Abstraction{}, action, value}},
+        _from,
+        state = %Agents.BlackjackAgent{}
+      ) do
     new_state = Map.put(state.state_value_table, inspect(env_state), %{inspect(action) => value})
     {:reply, new_state, %{state | state_value_table: new_state}}
+  end
+
+  def handle_call(
+        {:get_action, env_state},
+        _from,
+        state = %Agents.BlackjackAgent{state_value_table: Q}
+      ) do
+    [{action, __}] =
+      Q[inspect(env_state)] |> Enum.sort_by(fn {_, v} -> v end, &>=/2) |> Enum.take(1)
+
+    {:reply, action, state}
   end
 end
