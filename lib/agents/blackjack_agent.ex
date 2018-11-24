e7b1d193441c3a26d7f3f6e35a0f0fa0e7dbb9a2
Ricardo C <ricardo@suggestic.com>
2018-11-24 12:36:03 -0600

    implement q_get() and q_set() on maps instead of ets

--- lib/agents/blackjack_agent.ex
+++ lib/agents/blackjack_agent.ex
@@ -1,5 +1,6 @@
 defmodule Agents.BlackjackAgent do
   use GenServer
+  alias Env.Blackjack.Abstraction
   defstruct state_value_table: %{}, action_space: [0, 1]
 
   def init(_) do
@@ -10,27 +11,20 @@ defmodule Agents.BlackjackAgent do
     GenServer.start_link(__MODULE__, %Agents.BlackjackAgent{}, name: __MODULE__)
   end
 
-  def q_get(env_state = %Env.Blackjack{}, action) do
+  def q_get(env_state = %Abstraction{}, action) do
     GenServer.call(__MODULE__, {:q_get, {env_state, action}})
   end
 
-  def q_set(env_state = %Env.Blackjack{}, action, value) do
+  def q_set(env_state = %Abstraction{}, action, value) do
     GenServer.call(__MODULE__, {:q_set, {env_state, action, value}})
   end
 
-  def handle_call({:q_get, {env_state = %Env.Blackjack{}, action}}, _from, state) do
+  def handle_call({:q_get, {env_state = %Abstraction{}, action}}, _from, state) do
     {:reply, state.state_value_table[inspect(env_state)][inspect(action)], state}
   end
 
-  def handle_call({:q_set, {env_state = %Env.Blackjack{}, action, value}}, _from, state) do
-    new_state = %{
-      state.state_value_table
-      | inspect(env_state) => %{
-          state.state_value_table[inspect(env_state)]
-          | inspect(action) => value
-        }
-    }
-
+  def handle_call({:q_set, {env_state = %Abstraction{}, action, value}}, _from, state) do
+    new_state = Map.put(state.state_value_table, inspect(env_state), %{inspect(action) => value})
     {:reply, new_state, %{state | state_value_table: new_state}}
   end
 end
