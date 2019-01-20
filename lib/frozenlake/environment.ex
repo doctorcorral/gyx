bfabe4bf6aace93de0513f9a565c96f225f65c52
Ricardo C <ricardo@suggestic.com>
2019-01-19 21:20:28 -0600

    fix %Exp return for step clauses

--- lib/frozenlake/environment.ex
+++ lib/frozenlake/environment.ex
@@ -58,22 +58,22 @@ defmodule Gyx.FrozenLake.Environment do
 
   def handle_call({:act, :left}, _from, state) do
     new_env_state = %{state | col: max(state.col - 1, 0)}
-    {:reply, new_env_state, new_env_state}
+    {:reply, %Exp{}, new_env_state}
   end
 
   def handle_call({:act, :down}, _from, state) do
     new_env_state = %{state | row: min(state.row + 1, nrow(state.map) - 1)}
-    {:reply, new_env_state, new_env_state}
+    {:reply, %Exp{}, new_env_state}
   end
 
   def handle_call({:act, :right}, _from, state) do
     new_env_state = %{state | col: min(state.col + 1, state.ncol - 1)}
-    {:reply, new_env_state, new_env_state}
+    {:reply, %Exp{}, new_env_state}
   end
 
   def handle_call({:act, :up}, _from, state) do
     new_env_state = %{state | row: max(state.row - 1, 0)}
-    {:reply, new_env_state, new_env_state}
+    {:reply, %Exp{}, new_env_state}
   end
 
   defp nrow(map), do: length(map)
