1de43afbe87b2e5ff01d0e1f5598756b2f6ba001
Ricardo C <ricardo@suggestic.com>
2019-01-19 20:16:40 -0600

    change var naming for complain to gym

--- lib/frozenlake/environment.ex
+++ lib/frozenlake/environment.ex
@@ -4,13 +4,13 @@ defmodule Gyx.FrozenLake.Environment do
   use GenServer
   alias Experience.Exp
 
-  defstruct map: nil, x: nil, y: nil
-  @type t :: %__MODULE__{map: charlist, x: integer, y: integer}
+  defstruct map: nil, row: nil, col: nil
+  @type t :: %__MODULE__{map: list(charlist), row: integer, col: integer}
 
-  @actions  %{0 => :left, 1 => :down, 2 => :right, 3 => :up}
-  @action_space  Map.keys(@actions)
+  @actions %{0 => :left, 1 => :down, 2 => :right, 3 => :up}
+  @action_space Map.keys(@actions)
 
-  @maps  %{
+  @maps %{
     "4x4" => [
       "SFFF",
       "FHFH",
@@ -30,8 +30,8 @@ defmodule Gyx.FrozenLake.Environment do
   }
 
   @impl true
-  def init(map \\ "4x4") do
-    {:ok, %__MODULE__{map: map, x: 0, y: 0}}
+  def init(map_name \\ "4x4") do
+    {:ok, %__MODULE__{map: @maps[map_name], row: 0, col: 0}}
   end
 
   @impl Env
@@ -41,7 +41,7 @@ defmodule Gyx.FrozenLake.Environment do
 
   @impl true
   def handle_call(:reset, _from, state) do
-    new_env_state = %{state | x: 0, y: 0}
+    new_env_state = %{state | row: 0, col: 0}
     {:reply, %Exp{}, new_env_state}
   end
 end
