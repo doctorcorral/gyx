1a6e362b0af156de4617965b2c1a542e1d27c957
Ricardo C <ricardo@suggestic.com>
2019-02-02 22:55:45 -0600

    add test for get set on QGenServer

--- test/gyx_test.exs
+++ test/gyx_test.exs
@@ -1,8 +1,11 @@
 defmodule GyxTest do
   use ExUnit.Case
-  doctest Gyx
+  #doctest Gyx
 
-  test "greets the world" do
-    assert Gyx.hello() == :world
+  test "Q table | get set | GenSerer process" do
+    Gyx.Qstorage.QGenServer.q_set(%{a: 1}, 1, 13)
+    Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 2, 42)
+    Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 1, 11)
+    assert Gyx.Qstorage.QGenServer.q_get(%{a: 1}, 1) == 13
   end
 end
