d40ac1486c81caaaf1f5bf979dd2ede8f34df0ed
Ricardo C <ricardo@suggestic.com>
2019-02-03 12:40:59 -0600

    add QGenServer act/1 test

--- test/gyx_test.exs
+++ test/gyx_test.exs
@@ -1,11 +1,17 @@
 defmodule GyxTest do
   use ExUnit.Case
-  #doctest Gyx
 
-  test "Q table | get set | GenSerer process" do
+  setup do
     Gyx.Qstorage.QGenServer.q_set(%{a: 1}, 1, 13)
     Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 2, 42)
     Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 1, 11)
+  end
+
+  test "Q table | get | GenSerer process" do
     assert Gyx.Qstorage.QGenServer.q_get(%{a: 1}, 1) == 13
+  end
+
+  test "Q table | get_action | GenSerer process" do
+    assert Gyx.Qstorage.QGenServer.act(%{a: 2}) == 2
   end
 end
