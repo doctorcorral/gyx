1dbd23e8b967dbc661f67c783cd3d0e11e06af04
Ricardo C <ricardo@suggestic.com>
2019-02-04 11:56:19 -0600

    add type and @moduledoc on experience module

--- lib/blackjack/exp.ex
+++
@@ -1,3 +0,0 @@
-defmodule Experience.Exp do
-  defstruct state: nil, action: nil, reward: 0, next_state: nil, done: false, info: %{}
-end
---
+++ lib/framework/exp.ex
@@ -0,0 +1,28 @@
+defmodule Gyx.Experience.Exp do
+  @moduledoc """
+  This is data structure for representing an experience piece.
+  This is what is returned to an agent when interacting (calling `step/1`)
+  with the environment.
+
+  ### To consider
+  Usually, the experience pieces an agent gets from the environment, are
+  stored in a *replay buffer*, so the learning method can access to certain
+  experiences given a retrieval function.
+
+  These custom sampling techniques are responsability of the replay buffer module.
+
+  Use `info` key to store any additional metadata that could be useful for a
+  replay buffer to consider when sampling. For example, a timestamp that could
+  guarantee an atoic broadcasted replay buffer.
+  """
+  defstruct state: nil, action: nil, reward: 0, next_state: nil, done: false, info: %{}
+
+  @type t :: %__MODULE__{
+          state: any(),
+          action: number(),
+          reward: float(),
+          next_state: any(),
+          done: boolean(),
+          info: map()
+        }
+end
