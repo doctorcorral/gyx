64d25b8a9ec11f33966aaa9cb47fb97738f51988
Ricardo C <ricardo@suggestic.com>
2019-02-04 11:28:32 -0600

    add @moduledoc for Env behaviour

--- lib/framework/env.ex
+++ lib/framework/env.ex
@@ -1,4 +1,17 @@
 defmodule Gyx.Framework.Env do
+  @moduledoc """
+  This behaviour is intended to be followed for any `Environment` implementation
+  The most critical function to be exposed is `step/1` , which serves as a direct bridge
+  between the environment and any agent.
+
+  Here, an important design question to address is the fundamental difference between
+  the environment state (its internal representation) and an _observation_ of such state.
+
+  In principle, the environment returns an observation as part of step/1 response.
+
+  Should it be a way to obtain an evironment state abstraction as suposed to be shown
+  to an agent? i.e. an indirect observation.
+  """
   @callback reset() :: any()
   @callback get_state() :: any()
   @callback step(any()) :: any()
