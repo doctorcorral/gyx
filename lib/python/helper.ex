e65d84522090ccb6eeed7f8a2b71de96a9065db9
Ricardo C <ricardo@suggestic.com>
2019-02-09 23:46:52 -0600

    add default python instantiation #15

--- lib/python/helper.ex
+++ lib/python/helper.ex
@@ -16,9 +16,20 @@ defmodule Gyx.Python.Helper do
   @doc """
   Call python function using MFA format
   """
-  def call_python(pid, module, function, arguments \\ []) do
+  def call_python_pid(pid, module, function, arguments \\ []) do
     pid
     |>:python.call(module, function, arguments)
-
   end
+
+  def call_python(module, function, args \\ []) do
+    default_instance()
+    |> call_python_pid(module, function, args)
+  end
+
+  defp default_instance() do
+    path = [:code.priv_dir(:gyx), "python"]
+          |> Path.join()
+    python_instance(to_charlist(path))
+  end
+
 end
