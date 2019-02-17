65e30f6a3d0cfe9e087ec59767f11a701ec04b21
Ricardo C <ricardo@suggestic.com>
2019-02-17 12:54:06 -0600

    fix handle_call return value for clause :act_greedy

--- lib/agents/sarsa/sarsa_agent.ex
+++ lib/agents/sarsa/sarsa_agent.ex
@@ -9,9 +9,9 @@ defmodule Gyx.Agents.SARSA.Agent do
 
   def init(_) do
     {:ok,
-      %__MODULE__{
-        Q: QGenServer,
-      }}
+     %__MODULE__{
+       Q: QGenServer
+     }}
   end
 
   def start_link(_, opts) do
@@ -22,8 +22,7 @@ defmodule Gyx.Agents.SARSA.Agent do
     GenServer.call(__MODULE__, {:act_greedy, observation})
   end
 
-  def handle_call({:act_greedy, observation}, _from, %{Q: Q}) do
-    Q.get_max_action(observation)
+  def handle_call({:act_greedy, observation}, _from, %{Q: qtable} = state) do
+    {:reply, qtable.get_max_action(observation), state}
   end
-
 end
