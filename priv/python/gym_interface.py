523c4d5d3b84a2ef326e22cfc2a4085739ca3fdd
Ricardo C <ricardo@suggestic.com>
2019-02-25 16:10:43 -0600

    add erlport import

--- priv/python/gym_interface.py
+++ priv/python/gym_interface.py
@@ -1,4 +1,5 @@
 import gym
+from erlport.erlang import set_encoder, set_decoder
 
 def make(envname):
     print("🔥 🔥 🔥 Imporing Gym environment from Python:")
@@ -14,8 +15,14 @@ def step(env, _step):
     return (env, observation)
 
 def render(env):
+    #    env.env.ale.saveScreenPNG(b'test_image.png')
+    #res = env.render(mode='rgb_array')
     env.render()
+    
 
 def reset(env):
     initial_state = env.reset()
     return (env, initial_state)
+
+def action_space_sample(env):
+    return env.action_space.sample()
