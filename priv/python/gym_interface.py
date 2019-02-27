1fa84c00afe6952a188253b3e8d1bcfe3930c9aa
Ricardo C <ricardo@suggestic.com>
2019-02-27 13:05:31 -0600

    add action_space response #26

--- priv/python/gym_interface.py
+++ priv/python/gym_interface.py
@@ -1,6 +1,7 @@
 import gym
 from erlport.erlang import set_encoder, set_decoder
 
+
 def make(envname):
     print("🔥 🔥 🔥 Imporing Gym environment from Python:")
     en = str(envname, encoding='ascii')
@@ -8,21 +9,25 @@ def make(envname):
     print("🔥 🔥 🔥 😎")
     env = gym.make(en)
     initial_state = env.reset()
-    return (env, initial_state)
+    action_space = str(env.action_space).strip()
+    return (env, initial_state, action_space)
+
 
 def step(env, _step):
     observation = env.step(_step)
     return (env, observation)
 
+
 def render(env):
     #    env.env.ale.saveScreenPNG(b'test_image.png')
     #res = env.render(mode='rgb_array')
     env.render()
-    
+
 
 def reset(env):
     initial_state = env.reset()
-    return (env, initial_state)
+    return (env, initial_state, str(env.action_space).strip())
+
 
 def action_space_sample(env):
     return env.action_space.sample()
