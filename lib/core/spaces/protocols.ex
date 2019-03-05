20315edbacabc999dd2843e9c1cc03c198c17580
Ricardo C <ricardo@suggestic.com>
2019-03-05 15:19:24 -0600

    add Gyx.Core.Spaces.sample/1 protocol implementation for %Gyx.Core.Spaces.Box{} #33

--- lib/core/spaces/protocols.ex
+++ lib/core/spaces/protocols.ex
@@ -8,3 +8,17 @@ defimpl Gyx.Core.Spaces, for: Gyx.Core.Spaces.Discrete do
   end
 end
 
+defimpl Gyx.Core.Spaces, for: Gyx.Core.Spaces.Box do
+  def sample(box_space) do
+    random_action =
+      box_space.shape
+      |> Tuple.to_list()
+      |> Enum.map(&get_rands(&1, box_space))
+
+    {:ok, random_action}
+  end
+
+  defp get_rands(n, box_space) do
+    Enum.map(1..n, fn _ -> :rand.uniform() * (box_space.high - box_space.low) + box_space.low end)
+  end
+end
