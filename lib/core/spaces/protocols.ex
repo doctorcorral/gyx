b8ac86f950b71e6678b86126fd87d0a36e63271d
Ricardo C <ricardo@suggestic.com>
2019-03-05 22:01:25 -0600

    add @doc to Gyx.Core.Spaces protocol function sample/1 #33

--- lib/core/spaces/protocols.ex
+++ lib/core/spaces/protocols.ex
@@ -1,4 +1,28 @@
 defprotocol Gyx.Core.Spaces do
+  @moduledoc """
+  This protocol defines basic functions to interact with
+  action and observation spaces.
+  """
+
+  @doc """
+  Samples a random point from a space.
+  Note that sampled points are very different in nature
+  depending on the underlying space.
+  This sampling is pretty important for an agent, as
+  it is the way the agent might decide which actions to take
+  from an action space defined on the environment the agent is
+  interacting with.
+  ## Parameters
+
+    - space: Any module representing a space.
+
+  ## Examples
+      iex> Gyx.Core.Spaces.sample(%Gyx.Core.Spaces.Discrete{n: 42})
+      {:ok, 13}
+
+      iex> Gyx.Core.Spaces.sample(%Gyx.Core.Spaces.Box{shape: {2}, high: 7}
+      {:ok, [[3.173570417347619, 0.286615818442874]]}
+  """
   def sample(space)
 end
 
