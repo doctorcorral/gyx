9fc27510819b831609f2762c191b643b472d8d74
Ricardo C <ricardo@suggestic.com>
2019-03-06 12:34:27 -0600

    add @docmodule to Box and Discrete space modules #33

--- lib/core/spaces/discrete.ex
+++ lib/core/spaces/discrete.ex
@@ -1,9 +1,16 @@
 defmodule Gyx.Core.Spaces.Discrete do
+  @moduledoc  """
+  This space represents a set of `n` discrete options.
+  Thus, this space is represented by the number `n` of
+  available enumerable options.
+  Such options are assumed to be {0,1,...,n-1}.
+  """
 
-  defstruct n: nil, seed: {1,2,3}
+  defstruct n: nil, seed: {1,2,3}, random_algorithm: :exsplus
 
   @type t :: %__MODULE__{
           n: integer(),
+          random_algorithm: atom(),
           seed: {integer(), integer(), integer()}
         }
 end
