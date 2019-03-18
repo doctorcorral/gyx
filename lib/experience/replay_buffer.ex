d23331623fc5e63d241b7e619b2dcdfb267b45b6
Ricardo C <ricardo@suggestic.com>
2019-03-17 20:16:24 -0600

    add add/1 for adding experience to RB #36

--- lib/experience/replay_buffer.ex
+++ lib/experience/replay_buffer.ex
@@ -1,8 +1,9 @@
 defmodule Gyx.Experience.ReplayBuffer do
   use GenServer
+  alias Gyx.Core.Exp
 
   def start_link do
-    GenServer.start_link(__MODULE__,%{})
+    GenServer.start_link(__MODULE__, %{})
   end
 
   def init(state) do
@@ -14,8 +15,33 @@ defmodule Gyx.Experience.ReplayBuffer do
     GenServer.cast(__MODULE__, {:delete, key})
   end
 
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  def add(exp = %Exp{}) do
+    GenServer.cast(__MODULE__, {:add, exp})
+  end
+
+  def handle_cast({:add, exp}, state) do
+    {:ok, timestamp_key} = DateTime.now("Etc/UTC")
+    :ets.insert(:replay_buffer, {timestamp_key, exp})
+    {:noreply, state}
+  end
+
   def handle_cast({:delete, key}, state) do
     :ets.delete(:replay_buffer, key)
     {:noreply, state}
   end
+
+  def handle_call({:get, key}, _from, state) do
+    reply =
+      case :ets.lookup(:replay_buffer, key) do
+        [] -> nil
+        [{_timestamp, experience}] -> experience
+      end
+
+    {:reply, reply, state}
+  end
+
 end
