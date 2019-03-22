92644d2f9b1e7581dfc021ffc6829d74003b1272
Ricardo C <ricardo@suggestic.com>
2019-03-21 20:27:36 -0600

    add @moduledoc to Gyx.Core.ReplayMemory #36

--- lib/core/replay_memory.ex
+++ lib/core/replay_memory.ex
@@ -1,9 +1,18 @@
-defmodule ReplayMemory do
+defmodule Gyx.Core.ReplayMemory do
+  @moduledoc """
+  This module defines the behaviour for a Replay Memory.
+  The role of a Replay Memory is to store experiences coming from
+  one or multiple agents when interacting with their environments.
+  In this way, an agent can sample experiencess directly from a
+  replay memory with different strategies that can improve learning
+  convergence.
+  """
   @type experience :: Gyx.Core.Exp.t()
+  @type experiences :: list(experience)
 
   @callback add(experience()) :: any()
 
-  @callback get_random_batch(integer()) :: list(experience())
+  @callback get_random_batch(integer()) :: experiences()
 
   defmacro __using__(_params) do
     quote do
