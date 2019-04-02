4f174a05b94b3fc5deaef3062572214211a31a77
Ricardo C <ricardo@suggestic.com>
2019-04-01 23:51:41 -0600

    add contains for  test #40

--- test/core/spaces_test.exs
+++ test/core/spaces_test.exs
@@ -9,4 +9,12 @@ defmodule Gyx.Core.SpacesTest do
     assert Spaces.contains(discrete_space1, 8) == false
     assert Spaces.contains(discrete_space1, 6) == true
   end
+
+  test "Spaces | Box | contains" do
+    box_space = %Box{shape: {1, 2}}
+    {:ok, box_point} = Spaces.sample(box_space)
+    assert Spaces.contains(box_space, box_point) == true
+    assert Spaces.contains(box_space, [[0.5]]) == false
+    assert Spaces.contains(box_space, [[0.5], [0.5, 5.0]]) == true
+  end
 end
