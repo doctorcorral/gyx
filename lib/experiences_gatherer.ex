190b7b6895662f55f22e84a40eb5415fbb38b3a6
David Gomez Urquiza <david.gurquiza@gmail.com>
2019-05-13 19:56:44 -0500

    Sample experience gatherer worker

---
+++ lib/experiences_gatherer.ex
@@ -0,0 +1,5 @@
+defmodule Gyx.ExperiencesGatherer do
+  def learn(experience) do
+    IO.puts("Learned! #{experience}")
+  end
+end
--- lib/training_worker.ex
+++ lib/training_worker.ex
@@ -1,7 +1,11 @@
-defmodule TrainingWorker do
+defmodule Gyx.TrainingWorker do
   @behaviour Honeydew.Worker
 
   def train(seed) do
     IO.puts "Worker #{inspect(self())} - Starting training with seed: #{seed}!"
+    Process.sleep(seed * 500)
+    {:learn, ["Experience 1 from #{inspect self()}"]} |> Honeydew.async(:experiences) # Send experience
+    Process.sleep(seed * 1000)
+    {:learn, ["Experience 2 from #{inspect self()}"]} |> Honeydew.async(:experiences) # Send experience
   end
 end
