475650511402f06ff2e14e511071a48c9ea14f9c
Ricardo C <ricardo@suggestic.com>
2019-12-15 19:22:08 +0100

    refactor for calling env and agent processes

--- lib/trainers/trainer_sarsa.ex
+++ lib/trainers/trainer_sarsa.ex
@@ -9,9 +9,10 @@ defmodule Gyx.Trainers.TrainerSarsa do
 
   @enforce_keys [:environment, :agent]
 
-  defstruct environment: nil, agent: nil, trajectory: nil, rewards: nil
+  defstruct env_name: nil, environment: nil, agent: nil, trajectory: nil, rewards: nil
 
   @type t :: %__MODULE__{
+          env_name: String.t(),
           environment: any(),
           agent: any(),
           trajectory: list(Exp),
@@ -19,42 +20,54 @@ defmodule Gyx.Trainers.TrainerSarsa do
         }
 
   @env_module Gyx.Environments.Gym
-  #@env_module Gyx.Environments.Blackjack
-  @agent Gyx.Agents.SARSA.Agent
+  @agent_module Gyx.Agents.SARSA.Agent
 
-  def init(_) do
+  def init(env_name) do
+    {:ok, environment} = @env_module.start_link([], [])
+    {:ok, agent} = @agent_module.start_link([], [])
+
     {:ok,
      %__MODULE__{
-       environment: @env_module,
-       agent: @agent,
+       env_name: env_name,
+       environment: environment,
+       agent: agent,
        trajectory: [],
        rewards: []
-     }}
+     }, {:continue, :link_gym_environment}}
   end
 
-  def start_link(_, opts) do
-    GenServer.start_link(__MODULE__, [], opts)
+  def start_link(envname, opts) do
+    GenServer.start_link(__MODULE__, envname, opts)
   end
 
-  def train() do
-    GenServer.call(__MODULE__, :train)
+  def train(trainer, episodes) do
+    GenServer.call(trainer, {:train, episodes})
   end
 
-  def handle_call(:train, _from, t = %__MODULE__{}) do
-    {:reply, trainer(t, 500), t}
+  def handle_call({:train, episodes}, _from, t = %__MODULE__{}) do
+    {:reply, trainer(t, episodes), t}
+  end
+
+  def handle_continue(
+        :link_gym_environment,
+        state = %{env_name: env_name, environment: environment}
+      ) do
+    Gyx.Environments.Gym.make(environment, env_name)
+
+    {:noreply, state}
   end
 
   @spec trainer(__MODULE__.t(), integer) :: __MODULE__.t()
   defp trainer(t, 0), do: t
 
   defp trainer(t, num_episodes) do
-    t.environment.reset()
+    Gyx.Environments.Gym.reset(t.environment)
 
     t
     |> initialize_trajectory()
-    #|> IO.inspect(label: "Trajectory initialized")
+    # |> IO.inspect(label: "Trajectory initialized")
     |> run_episode(false)
-    #|> IO.inspect(label: "Episode finished")
+    # |> IO.inspect(label: "Episode finished")
     |> log_stats()
     |> trainer(num_episodes - 1)
   end
@@ -62,22 +75,24 @@ defmodule Gyx.Trainers.TrainerSarsa do
   defp run_episode(t = %__MODULE__{}, true), do: t
 
   defp run_episode(t = %__MODULE__{}, false) do
-    exp =
-      %Exp{done: done, state: s, action: a, reward: r, next_state: ss} =
-      %{
-        observation: t.environment.observe(),
-        action_space: t.environment.get_state().action_space
-      }
-      |> t.agent.act_epsilon_greedy()
-      |> t.environment.step
-
-    aa =
-      t.agent.act_epsilon_greedy(%{
-        observation: ss,
-        action_space: t.environment.get_state().action_space
+    next_action =
+      @agent_module.act_epsilon_greedy(t.agent, %{
+        current_state: observe(t.environment),
+        action_space: action_space(t.environment)
       })
 
-    t.agent.td_learn({s, a, r, ss, aa})
+    exp =
+      %Exp{done: done, state: s, action: a, reward: r, next_state: ss} =
+      t.environment
+      |> @env_module.step(next_action)
+
+    aa =
+      @agent_module.act_epsilon_greedy(t.agent,%{
+        current_state: ss,
+        action_space: action_space(t.environment)
+      })
+
+    @agent_module.td_learn(t.agent,{s, a, r, ss, aa})
     t = %{t | trajectory: [exp | t.trajectory]}
     run_episode(t, done)
   end
@@ -89,7 +104,11 @@ defmodule Gyx.Trainers.TrainerSarsa do
     t = %{t | rewards: [reward_sum | t.rewards]}
     k = 100
     Logger.info("Reward: " <> to_string((t.rewards |> Enum.take(k) |> Enum.sum()) / k))
-    Gyx.Qstorage.QGenServer.print_q_matrix()
+    # Gyx.Qstorage.QGenServer.print_q_matrix()
     t
   end
+
+  defp observe(environment), do: :sys.get_state(environment).current_state
+
+  defp action_space(environment), do: :sys.get_state(environment).action_space
 end
