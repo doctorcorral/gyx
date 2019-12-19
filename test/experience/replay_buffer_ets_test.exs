0371c7e63079184d2f6f4a71bd0765028e2b4460
Ricardo C <ricardo@suggestic.com>
2019-12-19 17:52:47 +0100

    refactor environment reference

--- test/experience/replay_buffer_ets_test.exs
+++ test/experience/replay_buffer_ets_test.exs
@@ -2,14 +2,18 @@ defmodule Gyx.Experience.ReplayBufferETSTest do
   use ExUnit.Case
   alias Gyx.Experience.ReplayBufferETS
 
+  @environment_module Gyx.Environments.Pure.Blackjack
+  @target_size 13
+
   setup do
     alias Gyx.Core.Spaces
-    environment = Gyx.Environments.Blackjack
+    environment = @environment_module.start_link([], [])
     action_space = environment.get_state().action_space
 
-    Enum.map(1..13, fn _ ->
+    Enum.map(1..@target_size, fn _ ->
       ReplayBufferETS.add(
-        environment.step(
+        @environment_module.step(
+          environment,
           with {:ok, action} <- Spaces.sample(action_space) do
             action
           end
@@ -21,6 +25,6 @@ defmodule Gyx.Experience.ReplayBufferETSTest do
   end
 
   test "Replay Buffer ETS | size" do
-    assert length(ReplayBufferETS.get_batch({20, :random})) == 13
+    assert length(ReplayBufferETS.get_batch({20, :random})) == @target_size
   end
 end
