1346d3fc75f8344c810e8ade00c2b2704e418d1c
Ricardo C <ricardo@suggestic.com>
2019-12-28 15:23:57 +0100

    refactor for propper q process setup and usage

--- test/qstorage/qgenserver_test.exs
+++ test/qstorage/qgenserver_test.exs
@@ -2,16 +2,18 @@ defmodule Gyx.Qstorage.QGenServerTest do
   use ExUnit.Case
 
   setup do
-    Gyx.Qstorage.QGenServer.q_set(%{a: 1}, 1, 13)
-    Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 2, 42)
-    Gyx.Qstorage.QGenServer.q_set(%{a: 2}, 1, 11)
+    {:ok, q_pid} = Gyx.Qstorage.QGenServer.start_link([], [])
+    Gyx.Qstorage.QGenServer.q_set(q_pid, %{a: 1}, 1, 13)
+    Gyx.Qstorage.QGenServer.q_set(q_pid, %{a: 2}, 2, 42)
+    Gyx.Qstorage.QGenServer.q_set(q_pid, %{a: 2}, 1, 11)
+    {:ok, %{q_pid: q_pid}}
   end
 
-  test "Q table | get | GenSerer process" do
-    assert Gyx.Qstorage.QGenServer.q_get(%{a: 1}, 1) == 13
+  test "Q table | get | GenSerer process", %{q_pid: q_pid} do
+    assert Gyx.Qstorage.QGenServer.q_get(q_pid, %{a: 1}, 1) == 13
   end
 
-  test "Q table | get_action | GenSerer process" do
-    assert Gyx.Qstorage.QGenServer.get_max_action(%{a: 2}) == {:ok, 2}
+  test "Q table | get_action | GenSerer process", %{q_pid: q_pid} do
+    assert Gyx.Qstorage.QGenServer.get_max_action(q_pid, %{a: 2}) == {:ok, 2}
   end
 end
